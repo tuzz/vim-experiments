8c9ec32a27354f22ba711ff72970820a7413157c
Chris Patuzzo <chris@patuzzo.co.uk>
2013-03-20 23:45:33 +0000

    Reason about the API

--- 14_orphans.rb
+++ 14_orphans.rb
@@ -4,3 +4,14 @@
 fork do
   Vim::Buffer.current.line = "Hello, subprocess?"
 end
+
+# This lets us reason about how the API works.
+#
+# Example, :ruby Vim::Buffer.current.line = "foo"
+#
+# 1. The API classes are initialized with Vim's current state.
+# 2. The body of the command is executed until the process exits.
+# 3. Vim inspects the state of the API classes and modifies Vim.
+#
+# Presumably, the API must push commands onto a FIFO queue.
+# You could write the entire thing without FFI, or any hacks. Smart.
